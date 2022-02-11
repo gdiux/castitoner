1eedcd88725bbd9c023260e5f5ee1cfc93f53cf5
Gilmer Contreras <adgdiux@gmail.com>
2022-02-11 14:37:57 -0400

    PDF

--- castitoner-frontend/src/app/pages/pedidos/pedidos.component.ts
+++ castitoner-frontend/src/app/pages/pedidos/pedidos.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
 // SERVICES
 import { UserService } from '../../services/user.service';
 import { PedidosService } from 'src/app/services/pedidos.service';
@@ -91,9 +94,6 @@ export class PedidosComponent implements OnInit {
     this.carrito = pedido.products;
     this.amountP = pedido.amount;
 
-    console.log(this.pedidoD);
-    
-
   }
 
   /** ================================================================
@@ -101,15 +101,44 @@ export class PedidosComponent implements OnInit {
   ==================================================================== */
   public desde:number = 0;
   cambiarPagina(valor: number){
-
+    
     this.desde += valor;    
-
+    
     if (this.desde < 0) {
       this.desde = 0;
     }
     this.cargarPedidos();
-
+    
   }
-  
 
+  /** ================================================================
+   *  PDF
+  ==================================================================== */
+  downloadPDF() {
+    // Extraemos el
+    const DATA = document.getElementById('htmlData') as HTMLElement;
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas( DATA , options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+  }
+
+
+  // FIN DE LA CLASE
 }
